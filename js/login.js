635d88200e0535dc36c6eb2b68d5fe517f4f9878
SergiiShtachenko <SergiiShtachenko@users.noreply.github.com>
2019-04-23 18:34:52 +0100

    Update_2019_04_22_22h00

---
+++ js/login.js
@@ -0,0 +1,62 @@
+// Timeout enquanto 3 vezes errar password
+let timeset = 0;
+let tentativas = 3;
+
+function contarTempo() {
+  let timer = setInterval(function() {
+      timeset -= 1000;
+  }, 1000); // decrementar 1 segundo
+  setTimeout(function() {
+    clearInterval(timer);
+    timeset = 0;
+    tentativas = 3;
+  }, 10000); // parar timer
+}
+
+
+// Ligação com a forma "userAccess"
+let form = document.getElementsByTagName('form')[0];
+
+// Função temporaria - validar credenciais sem php
+form.addEventListener('submit', function(event){
+    
+    let outMessage = document.getElementsByTagName('p')[0];
+
+    if (timeset === 0) {
+
+        let login = document.querySelector('form input[name=user]').value;
+        let password = document.querySelector('form input[name=password]').value;        
+                
+        let logCorretoArrey = new Array("teste", "admin", "user");
+        let passCorretoArrey = new Array("teste", "admin", "user");
+        
+        let index = logCorretoArrey.indexOf(login);
+        //alert(index);
+        
+        if(index < 0){
+            outMessage.innerHTML = "Introduziu o login errado";
+            document.querySelector('form input[name=user]').value = "";
+            document.querySelector('form input[name=password]').value = "";
+        } 
+        
+        else{
+            if (password === passCorretoArrey[index]) window.location.href = "mainMenu.html";
+            else{
+                if(tentativas > 1){ // 3 tentativas de inserir palavra passe
+                    outMessage.innerHTML = "Passe errada! Tente novamente!";
+                    tentativas--;
+                }
+                else{
+                    outMessage.innerHTML = "Tente novamente mais tarde!"
+                    timeset = 10000; // Esperar 10 segundos
+                    contarTempo();
+                }
+                document.querySelector('form input[name=password]').value = "";
+            }            
+        }
+
+    } else outMessage.innerHTML = "Excedeu limite das tentativas, tente novamente em " + (timeset / 1000) + " segundos";
+    
+
+    event.preventDefault();
+});
