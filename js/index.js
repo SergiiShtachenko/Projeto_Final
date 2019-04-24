046a0c87e25ac8a66730da5e8d20b2c3fb1e207a
SergiiShtachenko <SergiiShtachenko@users.noreply.github.com>
2019-04-24 19:17:33 +0100

    Update_2019_04_24_19h00

--- js/index.js
+++ js/index.js
@@ -1,11 +1,78 @@
+/*--------------------ANGULAR SETTING--------------------*/
 
+// Criar modulo angularApp
+var routerApp = angular.module('routerApp', ['ui.router']);
 
+routerApp.config(function($stateProvider, $urlRouterProvider) {
+    
+    $urlRouterProvider.otherwise('/login');
+    
+    $stateProvider
+        
+        // HOME STATES AND NESTED VIEWS ========================================
+        .state('login', {
+            url: '/login',
+            templateUrl: 'login.html'
+        })
+        
+        // nested list with custom controller
+        .state('home.list', {
+            url: '/list',
+            templateUrl: 'partial-home-list.html',
+            controller: function($scope) {
+                $scope.dogs = ['Bernese', 'Husky', 'Goldendoodle'];
+            }
+        })
+        
+        // nested list with just some random string data
+        .state('home.paragraph', {
+            url: '/paragraph',
+            template: 'I could sure use a drink right now.'
+        })
+        
+        // ABOUT PAGE AND MULTIPLE NAMED VIEWS =================================
+        .state('about', {
+            url: '/about',
+            views: {
+                '': { templateUrl: 'partial-about.html' },
+                'columnOne@about': { template: 'Look I am a column!' },
+                'columnTwo@about': { 
+                    templateUrl: 'table-data.html',
+                    controller: 'scotchController'
+                }
+            }
+            
+        });
+        
+});
 
+routerApp.controller('scotchController', function($scope) {
+    
+    $scope.message = 'test';
+   
+    $scope.scotches = [
+        {
+            name: 'Macallan 12',
+            price: 50
+        },
+        {
+            name: 'Chivas Regal Royal Salute',
+            price: 10000
+        },
+        {
+            name: 'Glenfiddich 1937',
+            price: 20000
+        }
+    ];
+    
+});
+
+/*--------------------FIM ANGULAR SETTING--------------------*/
 
 // Função temporaria - atribuir o nome do utilizador
-let form = document.getElementsByTagName('form')[0];
+let form1 = document.getElementsByTagName('form')[0];
 
-form.addEventListener('submit', function(event){
+form1.addEventListener('submit', function(event){
 
     let nome = document.getElementById("userName");    
     nome.innerHTML = document.querySelector('form input[name=user]').value;
