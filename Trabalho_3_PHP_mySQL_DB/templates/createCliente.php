5c696ae31f7d68f505e7565fbea8cf7aa224bf23
soarespedro84 <48367290+soarespedro84@users.noreply.github.com>
2019-06-23 19:02:21 +0100

    Add files via upload

---
+++ Trabalho_3_PHP_mySQL_DB/templates/createCliente.php
@@ -0,0 +1,25 @@
+
+
+          <link rel="stylesheet" href="../css/registo.css">
+          <script src="js/registo.js" defer></script>
+
+  <!--Pásina de Registo-->
+      <section class="RegistoNovoCliente">
+          <article id="NovoCliente">
+              <form name="registoCliente" action="novoCliente.php" method="POST">                  
+                  <p class="Dados">
+                      <input type="text" name="nome" placeholder="Nome da Empresa *" required="preenchimento obrigatório" ></p>
+                      <p><input type="text" name="nif" placeholder="NIF *" required="preenchimento obrigatório" ></p>
+                  <p class="Morada">
+                      <input type="text" name="Morada" placeholder="Morada da empresa *" required="preenchimento obrigatório" ></p>
+                  <p class="contacto">
+                      <p><input type="tel" name="Telefone" placeholder="contacto telefonico*" required="preenchimento obrigatório" ></p>
+                  <h5>*campos de preenchimento obrigatório</h5>
+                  <p><input type="submit" value="Enviar"></p>                
+              </form>
+          </article>
+      </section>    
+      
+  
+     
+
