0b5dbf793fde9119224a6bc16944557d71383869
soarespedro84 <48367290+soarespedro84@users.noreply.github.com>
2019-06-24 11:05:55 +0100

    Add files via upload

--- Trabalho_3_PHP_mySQL_DB/database/novoCliente.php
+++ Trabalho_3_PHP_mySQL_DB/database/novoCliente.php
@@ -2,12 +2,13 @@
 
   function saveNewClient(){
 
-    include ('config/init_p.php');
+    include ('../config/init_p.php');
 
     $nomeCliente=$_POST['nome'];
     $nifCliente=$_POST['nif'];
     $moradaCliente=$_POST['Morada'];
     $telefoneCliente=$_POST['Telefone'];
+    
     
     $stmt = $dblink->prepare("INSERT INTO cliente (nome, nif, morada, telefon) VALUES ('$nomeCliente', '$nifCliente','$moradaCliente', '$telefoneCliente');");
     
