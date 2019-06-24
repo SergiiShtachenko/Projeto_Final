b0ab8a4fe60107bc9297dfa58fb4aaceb6ddbf2e
soarespedro84 <48367290+soarespedro84@users.noreply.github.com>
2019-06-24 15:58:37 +0100

    Add files via upload

--- Trabalho_3_PHP_mySQL_DB/database/usersList.php
+++ Trabalho_3_PHP_mySQL_DB/database/usersList.php
@@ -1,7 +1,8 @@
 <?php
     //database
     function getAllUtilizador(){
-  
+        include ('../config/init_p.php');
+        include ('../models/utilizador.php');
 
         $stmt = $dblink->prepare('SELECT * FROM utilizador');
         $stmt->execute();
@@ -10,8 +11,10 @@
         
         while($row = $stmt->fetch()){
             $listaUser[] = new Utilizador($row['guid'], $row['email'], $row['palavrapasse'], $row['nome'], $row['telefon'], $row['cliente'],  $row['permissao'], $row['ativo']);
+            //$listaUser=array($row['guid'], $row["email"], $row["palavrapasse"], $row["nome"], $row["telefon"], $row["cliente"], $row["permissao"], $row["ativo"]); 
+            
+        
         }
         return $listaUser;
-    }
+    }            
 ?>
-   
