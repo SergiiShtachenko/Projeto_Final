4c6f134144838297f301810bb1e77945c5390d07
soarespedro84 <48367290+soarespedro84@users.noreply.github.com>
2019-06-24 16:01:07 +0100

    Add files via upload

---
+++ Trabalho_3_PHP_mySQL_DB/listaClientsVer.php
@@ -0,0 +1,13 @@
+<?php 
+    //include ('config/init_p.php');
+    //include ('models/utilizador.php');
+    //include ('templates/cl_header.php');
+    include ('database/clientList.php');
+
+    $listaClientsVer = getAllClients();
+
+    
+    //include ('templates/cl_produtoList.php');
+    //include ('templates/footer.php');
+    
+?>
