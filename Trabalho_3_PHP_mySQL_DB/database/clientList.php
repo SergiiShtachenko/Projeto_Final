ca033c759a5c917b5df328ea3ac70f753cc4b707
soarespedro84 <48367290+soarespedro84@users.noreply.github.com>
2019-06-24 15:58:15 +0100

    Add files via upload

---
+++ Trabalho_3_PHP_mySQL_DB/database/clientList.php
@@ -0,0 +1,18 @@
+<?php
+    //database
+    function getAllClients(){
+        include ('../config/init_p.php');
+        include ('../models/cliente.php');
+
+        $stmt = $dblink->prepare('SELECT * FROM cliente');
+        $stmt->execute();
+
+        //return $stmt->fetchAll();
+        
+        while($row = $stmt->fetch()){
+            $listaClients[] = new Cliente($row['guid'], $row['nrcliente'], $row['nome'], $row['nif'], $row['morada'], $row['telefon'], $row['ativo']);
+            
+        }
+        return $listaClients;
+    }            
+?>
