fd8e0d94aeade960c72f1c84c1ec3d99232e183e
soarespedro84 <48367290+soarespedro84@users.noreply.github.com>
2019-06-25 01:34:05 +0100

    Add files via upload

---
+++ Trabalho_3_PHP_mySQL_DB/clientsList.php
@@ -0,0 +1,40 @@
+<link rel="stylesheet" href="css/clients.css">
+<link rel="stylesheet" href="css/header.css">
+    <script src="js/clients.js"></script>
+<!-------fim header------->
+<?php  include ('listaClientsVer.php'); ?>
+<!-------lista_utilizadores.php------->
+<section>
+<br>
+   <br> 
+  <h1>Clientes</h1>
+  <table id="clients">
+  <tr>
+  <th><input type="text" id="Nome" onkeyup="procuraNome()" placeholder="Nome"></th>
+  <th><input type="text" id="Morada" onkeyup="procuraPais()" placeholder="Morada"></th>
+  <th><input type="text" id="NIF" onkeyup="procuraNIF()" placeholder="NIF"></th> 
+  <th><input type="text" id="Telefone" onkeyup="procuraNIF()" placeholder="Telefone"></th>
+  <th><input type="submit" value="Pesquisar"></th>            
+  
+  </tr>
+<?php
+   
+    foreach($listaClientsVer as $item){
+        //if($item->getAtivo()){
+?>
+
+
+    <table class="clientList">
+        <tr><td><h3><?php echo $item->getNomeCliente(); ?></h3></td>
+                <td><?php echo $item->getNif(); ?></td>
+                <td><?php echo $item->getMorada(); ?></td>                      
+                <td><?php echo $item->getTelefon(); ?></td>
+                <td><input type="submit" value="Editar"></td>
+                
+        </tr>
+    </table>
+</section>
+<?php
+       // }
+    }
+?>
